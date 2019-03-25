7afd79e58b063cf8a4a532586d735901da1b3624
Sayani07 <Sayani.gupta@monash.edu>
2019-03-26 10:43:27 +1100

    added d_sem and w_sem

--- R/access_week.R
+++ R/access_week.R
@@ -21,26 +21,28 @@ first_day_of_month_wday <- function(dy) {
   first_day_month = paste0(y,"-",m,"-","01")
   wday(first_day_month)
 }
+
+sem_day <- function(x) {
+   which_sem <- semester(x)
+   day_x <- yday(x)
+   year_leap <- leap_year(x)
+   div_indx <- if_else(year_leap == "FALSE",182, 183)
+   if_else(which_sem==1,day_x, day_x - div_indx + 1)
+}
+
 #' @export
 w_m = function(x)
 {
   ceiling((day(x) + first_day_of_month_wday(x) - 1) / 7)
 }
 
-
-w_q  =  function(x)
+w_q = function(x)
 {
   ceiling((qday(x)) / 7)
 }
 
-# w_sem = function(x)
-#
-# {
-#   ceiling((day(x) + first_day_of_month_wday(x) - 1) / 7)
-# }
-
-w_y = function(x)
-
+w_sem = function(x)
 {
-  week(x)
+  ceiling((sem_day(x))/ 7)
 }
+
