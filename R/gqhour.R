f7ae1ac521973515eb4a9ae0a1bfbe2773d1a9de
Sayani07 <Sayani.gupta@monash.edu>
2019-04-09 08:12:59 +1000

    fixed - working fine now - need more testing

--- R/gqhour.R
+++ R/gqhour.R
@@ -12,7 +12,7 @@
 #' @author Sayani Gupta
 #' @examples
 #' \dontrun{
-#' tsibbledata::aus_elec %>% mutate(hour_day = gqhour(Time, "hour")) %>% tail()
+#' tsibbledata::aus_elec %>% mutate(qhour_day = gqhour(Time, "hour")) %>% tail()
 #' gqtrhour(lubridate::now(),"day")
 #' }
 #' @export gqhour
@@ -20,7 +20,7 @@ gqhour <- function(x, granularity = "day")
 {
   # match the gran_type
   gran_lower <- tolower(granularity)
-  gran_pos <-  c("hhour", "hour","day","week", "month", "quarter", "semester", "year")
+  gran_pos <-  c("hhour", "hour","day", "semester","week", "month", "quarter", "year")
 
   #check if the user input is correct
   if(!gran_lower  %in% gran_pos)
@@ -28,16 +28,17 @@ gqhour <- function(x, granularity = "day")
     stop(paste("granularity", gran_lower, "is not one of hhour, hour, day, week, month, quarter, semester or year", sep = " "), call.=F)
   }
 
+
   # match the gran_type
-  gran_type <- match.arg(gran_lower, choices = gran_pos, several.ok = TRUE)
+  #gran_type <- match.arg(gran_lower, choices = gran_pos, several.ok = TRUE)
 
-  gran_type_indx <- match(gran_type, gran_pos)
+  gran_type_indx <- match(gran_lower, gran_pos)
 
-  lubridate_match <- c("wday","day", "qday","semester","yday")
+  lubridate_match <- c("wday","day", "qday","yday")
 
   if(gran_type_indx==1)
   {
-    gqhour_value <- qh_h(x)%%2 + 1
+    gqhour_value <- ceiling(qh_h(x)%%2)
   }
   else if(gran_type_indx==2)
   {
@@ -47,28 +48,31 @@ gqhour <- function(x, granularity = "day")
   {
     gqhour_value = qh_d(x)
   }
+  else if (gran_type_indx ==4)
+  {
+    gqhour_value = qh_d(x) + 24*4*(d_sem(x) - 1)
+  }
+
   else
   {
+    match_value <- eval(parse(text = paste0("lubridate::",lubridate_match[gran_type_indx - 4],"(x)")))
 
-    match_value <- eval(parse(text = paste0("lubridate::",lubridate_match[gran_type_indx - 3],"(x)")))
-    gqhour_value <- qh_d(x) + 96*(match_value - 1)
+    gqhour_value <- qh_d(x) + 24*4*(match_value - 1)
   }
 
   return(gqhour_value)
-
 }
-
 
 qh_h <- function(x) {
 
   # finds which quarter of the hour
-  floor(lubridate::minute(x)/15) + 1
+  ceiling(lubridate::minute(x)/15)
 
 }
 
 qh_d <- function(x) {
 
   # finds which quarter of the day
-  lubridate::hour(x)*qh_h(x) + qh_h(x)
+   qh_h(x) + 4*(lubridate::hour(x))
 
 }
