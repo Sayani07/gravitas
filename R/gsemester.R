abf278d387e0de3a989e5de7714eb45b00dd6e21
Sayani07 <Sayani.gupta@monash.edu>
2019-04-09 09:29:54 +1000

    gsemester update with other arguments parsed

---
+++ R/gsemester.R
@@ -0,0 +1,38 @@
+#' Get combination of semester component of a date time
+#'
+
+#' Date-time must be a  POSIXct, POSIXlt, Date, Period, chron, yearmon, yearqtr, zoo,
+#' zooreg, timeDate, xts, its, ti, jul, timeSeries, and fts objects.
+#'
+
+#' @param x a date-time object
+#' @param granularity the granularity to be paired up with semester
+#' @return combination of the semester component of x as a number
+#
+#' @author Sayani Gupta
+#' @examples
+#' \dontrun{
+#' tsibbledata::aus_elec %>% mutate(semester_year = gsemester(Time, "year")) %>% tail()
+#' }
+#' @export gsemester
+gsemester <- function(x, granularity = "year",...)
+{
+  # match the gran_type
+  gran_lower <- tolower(granularity)
+
+  #check if the user input is correct
+  if(!gran_lower  %in% c("year"))
+  {
+    stop(paste("granularity", gran_lower, "is not year", sep = " "), call.=F)
+  }
+
+  # match the gran_type
+  gran_type <- match.arg(gran_lower, choices = c("year"))
+
+  if(gran_type=="year")
+  {
+    gsemester_value <-  lubridate::semester(x,...)
+  }
+  return(gsemester_value)
+
+}
