1e7e752ad039c3da5ea30b07a4f0ace745ff8cc4
Sayani07 <Sayani.gupta@monash.edu>
2019-04-26 17:30:16 +1000

    consistent changes done according to ghour

--- R/ghhour.R
+++ R/ghhour.R
@@ -14,53 +14,41 @@
 #' @examples
 #' \dontrun{
 #' tsibbledata::aus_elec %>% mutate(hh_day = ghalfhour(Time, "day")) %>% tail()
-#' ghalfhour(lubridate::now(),"week")
+#' ghalfhour(lubridate::now(), "week")
 #' }
 #' @export ghalfhour
-ghalfhour <- function(x, granularity = "hour",...)
-{
+ghhour <- function(x, granularity = "hour", ...) {
   # match the gran_type
   gran_lower <- tolower(granularity)
+  gran_opt <- c("hour", "day", "week", "month", "quarter", "semester", "year")
 
-  #check if the user input is correct
-  if(!gran_lower  %in% c("hour","day","week", "month", "quarter", "semester", "year"))
-  {
-    stop(paste("granularity", gran_lower, "is not one of hour, day, week, month, quarter, semester or year", sep = " "), call.=F)
+  # check if the user input is correct
+  if (!gran_lower %in% gran_opt) {
+    stop(paste0("granularity", gran_lower, "is not one of", paste0(gran_opt, collapse = ",")), call. = F)
   }
 
-  # match the gran_type
-  gran_pos <-  c("hour","day","week", "month", "quarter", "semester", "year")
+  gran_type <- match.arg(gran_lower, choices = gran_opt, several.ok = TRUE)
 
-  gran_type <- match.arg(gran_lower, choices = gran_pos, several.ok = TRUE)
+  gran_type_indx <- match(gran_type, gran_opt)
 
-  gran_type_indx <- match(gran_type, gran_pos)
-
-  # there is no function in lubrdiate which computes half hour of the hour or half hour of the day
-  lubridate_match <- c("wday", "day", "qday","semester","yday")
+  lubridate_match <- c("na","na","wday", "day", "qday", "na", "yday")
 
 
-  if(gran_type_indx==1)
-  {
-    ghalfhour_value <- dplyr::if_else(lubridate::minute(x) <30, 1, 2)
+  if (gran_type == "hour") {
+    ghalfhour_value <- dplyr::if_else(lubridate::minute(x) < 30, 1, 2)
   }
-  else if(gran_type_indx==2)
-  {
+  else if (gran_type == "day") {
     ghalfhour_value <- hh_d(x)
   }
-  else
-  {
-
-    match_value <- eval(parse(text = paste0("lubridate::",lubridate_match[gran_type_indx - 2],"(x)")))
-    ghalfhour_value <- hh_d(x) + 48*(match_value - 1)
+  else {
+    match_value <- eval(parse(text = paste0("lubridate::", lubridate_match[gran_type_indx], "(x)")))
+    ghalfhour_value <- hh_d(x) + 48 * (match_value - 1)
   }
 
   return(ghalfhour_value)
-
 }
 
 
-hh_d = function(x)
-{
-  (lubridate::hour(x)*60 + lubridate::minute(x))/30
+hh_d <- function(x) {
+  (lubridate::hour(x) * 60 + lubridate::minute(x)) / 30
 }
-
