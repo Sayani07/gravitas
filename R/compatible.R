7c72fe78e16f089bb321fb6451761961968153c3
Sayani07 <Sayani.gupta@monash.edu>
2019-04-30 11:23:04 +1000

    L1, L2 correction

--- R/compatible.R
+++ R/compatible.R
@@ -12,22 +12,21 @@
 #' @param argfn2 granularity to be fed in fn2
 #'
 #' @return compatibility table providing if the two granularities are harmonies or clashes. It also provides information on the range of the number of observations per combination and variation across number of combinations and other summery statistics.
-#
+
 #' @examples
 #' \dontrun{
-#' compatible_table(tsibbledata::aus_elec, gday, "week", ghour, "day" )
-#' @export compatible_table
+#' tsibbledata::aus_elec %>% dplyr::mutate(hour_day = ghour(Time, "day")) %>% tail()
+#' }
 
-compatible_table <-  function(data, fn1, argfn1 = "week", fn2, argfn2 = "day")
-{
-  if(!tsibble::is_tsibble(data))
-  {
+compatible_table <- function(data, fn1, argfn1 = "week", fn2, argfn2 = "day") {
+  if (!tsibble::is_tsibble(data)) {
     stop("must use tsibble")
   }
 
-  ind =  data[[as_string(tsibble::index(data))]]
 
-  arg_opt = c("minute", "qhour", "hhour", "hour", "day", "week", "month", "quarter", "semester", "year")
+  ind <- data[[rlang::as_string(tsibble::index(data))]]
+
+  arg_opt <- c("minute", "qhour", "hhour", "hour", "day", "week", "month", "quarter", "semester", "year")
 
   argfn1 <- tolower(argfn1)
   argfn2 <- tolower(argfn2)
@@ -36,24 +35,25 @@ compatible_table <-  function(data, fn1, argfn1 = "week", fn2, argfn2 = "day")
     stop(paste0("argument is not one of ", paste0(arg_opt, collapse = ", ")), call. = F)
   }
 
-  mk_data = data %>% mutate(L1 = fn1(ind, argfn1),
-                            L2 = fn2(ind, argfn2))
+  mk_data <- data %>% dplyr::mutate(
+    L1 = fn1(ind, argfn1),
+    L2 = fn2(ind, argfn2)
+  )
 
-  #All possible combinations that are possible
+  # All possible combinations that are possible
   Allcomb <- tidyr::expand(mk_data, L1, L2)
-  #All possible combinations that  exist
+  # All possible combinations that  exist
   combexist <- tidyr::expand(mk_data, tidyr::nesting(L1, L2))
-  #All possible combination that are missing
-  cmbmiss <- Allcomb %>% anti_join(combexist)
+  # All possible combination that are missing
+  cmbmiss <- Allcomb %>% dplyr::anti_join(combexist)
 
-  Type = if_else(nrow(cmbmiss)!= 0, "Clashes", "Harmonies")
-  Obs_combinations = mk_data %>% as_tibble() %>% group_by(L1, L2) %>% count()
-  Obs_comb_min = range(Obs_combinations$n)[1]
-  Obs_comb_max = range(Obs_combinations$n)[2]
-  Obs_comb_var = round(sd(Obs_combinations$n)/mean(Obs_combinations$n), digits = 3)
+  Type <- dplyr::if_else(nrow(cmbmiss) != 0, "Clashes", "Harmonies")
+  Obs_combinations <- mk_data %>% tibble::as_tibble() %>% dplyr::group_by(L1, L2) %>% dplyr::tally()
+  Obs_comb_min <- range(Obs_combinations$n)[1]
+  Obs_comb_max <- range(Obs_combinations$n)[2]
+  Obs_comb_var <- round(stats::sd(Obs_combinations$n) / mean(Obs_combinations$n), digits = 3)
 
-  Output = tibble(Type, `Min Obs` = Obs_comb_min, `Max Obs` = Obs_comb_max, `CV` = Obs_comb_var)
+  Output <- tibble::tibble(Type, `Min Obs` = Obs_comb_min, `Max Obs` = Obs_comb_max, `CV` = Obs_comb_var)
 
   Output
 }
-
