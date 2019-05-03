57730ebfebd676e0ac10908f9d6ea0cd4af2176b
Sayani07 <Sayani.gupta@monash.edu>
2019-05-03 13:33:01 +1000

    lookup function added

---
+++ R/lookup.R
@@ -0,0 +1,12 @@
+lookup_tbl <- function(gran)
+{
+  # edit this list if you add granularities
+  granularity = c("second", "minute", "qhour","hhour", "hour", "day", "week", "fortnight", "month",    "quarter", "semester", "year")
+
+  # edit this list if you add granularities
+  lubridate_value = c(NA, NA, NA, NA, NA, NA, "wday",NA,  "day", "qday", "na", "yday")
+
+  m = match(gran, granularity)
+
+  lubridate_value[m]
+  }
