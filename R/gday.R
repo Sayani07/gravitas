2294931c63b226d3dbe805d0617d2c17663da20d
Sayani07 <Sayani.gupta@monash.edu>
2019-05-14 08:48:48 +1000

    Modified code consistent with ghour

--- R/gday.R
+++ R/gday.R
@@ -12,28 +12,19 @@
 #' gday(lubridate::now(), "month")
 #' @export gday
 gday <- function(x, granularity = "week", ...) {
-  gran_lower <- tolower(granularity)
+  # lookup_tbl to be used for gran hour
+  lookup_l1 <- lookup_tbl("day")
 
-  gran_opt <- c("week", "month", "quarter", "semester", "year")
-
+  # Pick up the possible granularities from lookup table
+  gran_opt <- lookup_l1$gran_possible
 
   # check if the user input is correct
-  if (!gran_lower %in% gran_opt) {
-    stop(paste0("granularity ", gran_lower, " is not one of ", paste0(gran_opt, collapse = ", ")), call. = F)
-  }
+  gran_type <- match.arg(granularity, choices = gran_opt, several.ok = TRUE)
 
-  gran_type <- match.arg(gran_lower, choices = gran_opt, several.ok = TRUE)
+  # Match the input granularity from the lookup_tbl
+  lookup_l2 <-  lookup_tbl(granularity)$match_day
 
-  lubridate_match <- c("wday", "day", "qday", "na", "yday")
+  gday_value <- eval(parse_exp(lookup_l2))
 
-  gran_type_indx <- match(gran_type, gran_opt)
-
-  if (!gran_type == "semester") {
-    match_value <- eval(parse(text = paste0("lubridate::", lubridate_match[gran_type_indx], "(x, ...)")))
-  }
-  else {
-    match_value <- d_sem(x)
-  }
-
-  return(match_value)
+  return(gday_value)
 }
