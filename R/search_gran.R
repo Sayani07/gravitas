60a292ddccc2eae96e94bc7387eb5d117bae1b12
Sayani07 <Sayani.gupta@monash.edu>
2019-07-16 16:30:33 +1000

    sorted the sequence error while inputting filter_in and filter_out

--- R/search_gran.R
+++ R/search_gran.R
@@ -1,16 +1,19 @@
 #' Get set of temporal granularities for a given tsibble
 #'
 #' @param .data a tsibble object
-#' @param ugran upper granularity
+#' @param ugran upper temporal unit
+#' @param lgran lowest temporal unit
+#' @param filter_in choices of temporal units to be kept
+#' @param filter_out choices of temporal units to be discarded
 #' @param ... added arguments to be passed
 #' @return set of granularities
 #' @examples
 #' library(dplyr)
 #' library(tsibbledata)
-#' tsibbledata::gafa_stock %>% harmony(lgran = "hour", ugran = "week")
-#' tsibbledata::vic_elec %>% harmony(ugran = "day")
+#' tsibbledata::vic_elec %>% search_gran(ugran = "month", filter_out = c("hhour","fortnight"))
+#'tsibbledata::gafa_stock %>% search_gran(ugran = "month",lgran = "week")
 #' @export search_gran
-search_gran <- function(.data, ugran = "year", lgran = NULL, ...){
+search_gran <- function(.data, ugran = "year", lgran = NULL, filter_in =  NULL, filter_out = NULL, ...){
 granularity <- lookup_table$granularity
 constant <- lookup_table$constant
 
@@ -62,26 +65,36 @@ index_gran2 <- granularity %>% match(x = ugran)
 gran2_set <- lookup_table$granularity[index_gran1:index_gran2]
 
 
+gran <- paste(gran1 = combn(gran2_set, 2)[1, ], gran2 = combn(gran2_set, 2)[2, ], sep = "_")
 
-set1 <- paste(gran1 = combn(gran2_set, 2)[1, ], gran2 = combn(gran2_set, 2)[2, ], sep = "_")
+gran_split <- stringr::str_split(gran, "_", 2) %>% unlist() %>% unique()
 
-return(set1)
-}
 
-filter_gran <- function(.data, ugran = "year", lgran = NULL, filter_in =  NULL, filter_out = NULL, ...){
-  gran <- search_gran(.data, ugran, lgran,...)
-  gran_split <- stringr::str_split(gran, "_", 2) %>% unlist() %>% unique()
-
-  if(!is.null(filter_in)){
-    gran_split <- gran_split[match(filter_in, gran_split)]
-    gran <- paste(gran1 = combn(gran_split, 2)[1, ], gran2 = combn(gran_split, 2)[2, ], sep = "_")
+if(!is.null(filter_in)){
+  if(length(filter_in)==1){
+    stop("Atleast two temporal units to be provided for filter_in ")
+  }
+  if(!all(filter_in %in% granularity)){
+    stop("temporal units to be filtered in not found: make sure vector contains units which are between lgran and ugran")
   }
 
-  else if(!is.null(filter_out))
-  {
-    gran_split <- gran_split[-match(filter_out, gran_split)]
-    gran <- paste(gran1 = combn(gran_split, 2)[1, ], gran2 = combn(gran_split, 2)[2, ], sep = "_")
-  }
-
-  return(gran)
+  filter_in <- filter_in[match(granularity, filter_in)]
+  filter_in <-  filter_in[!is.na(filter_in)]
+  gran_split <- gran_split[match(filter_in, gran_split)]
+  gran <- paste(gran1 = combn(gran_split, 2)[1, ], gran2 = combn(gran_split, 2)[2, ], sep = "_")
 }
+
+else if(!is.null(filter_out))
+{
+  if(!all(filter_out %in% granularity)){
+    stop("temporal units to be filtered out not found: make sure vector contains units which are between lgran and ugran")
+  }
+  filter_out <- filter_out[match(granularity, filter_out)]
+  filter_out <-  filter_out[!is.na(filter_out)]
+  gran_split <- gran_split[-match(filter_out, gran_split)]
+  gran <- paste(gran1 = combn(gran_split, 2)[1, ], gran2 = combn(gran_split, 2)[2, ], sep = "_")
+}
+
+return(gran)
+}
+
