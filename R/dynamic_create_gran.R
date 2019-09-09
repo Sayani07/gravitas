d6b6209284b8af2d545d198c9cacdef689bfb267
Sayani07 <Sayani.Gupta@monash.edu>
2019-09-09 23:37:09 +1000

    adding dynamic create gran

--- R/dynamic_create_gran.R
+++ R/dynamic_create_gran.R
@@ -1,4 +1,4 @@
-#' Build temporal granularities
+#' Build dynamic temporal granularities
 
 #' @param .data A tsibble object.
 #' @param gran the required granularity.
@@ -11,6 +11,61 @@
 #' library(tsibble)
 #' tsibbledata::vic_elec %>% as_tsibble() %>% create_gran("hour_week") %>% tail()
 #' @export dynamic_create_gran
+#'
+dynamic_create_gran <-  function(.data, hierarchy_tbl = NULL, gran = NULL, verify_col = FALSE, ...)
+{
+
+  x = .data[[tsibble::index(.data)]] # index column
+
+  if(any(class(x) %in% c("POSIXct", "POSIXt")))
+    value = create_gran(.data, gran,...)
+  else
+  {
+    gran_split <- stringr::str_split(gran, "_", 2) %>% unlist() %>% unique()
+    lgran = gran_split[1]
+    ugran = gran_split[2]
+
+    lgran_ordr1 <- dynamic_g_order(hierarchy_tbl, lgran, order = 1)
+
+    gran_init <- paste(gran_split[1], lgran_ordr1, sep="_")
+    gran_final <- paste(lgran_ordr1, gran_split[2], sep="_")
+
+    if (dynamic_g_order(hierarchy_tbl, lgran, gran_split[2]) == 1) {
+      value =  create_single_gran(.data, hierarchy_tbl, gran)
+    }
+    else {
+      value <- dynamic_create_gran(.data, hierarchy_tbl, gran_init) +
+        dynamic_gran_convert(hierarchy_tbl, lgran, lgran_ordr1) *
+        (dynamic_create_gran(.data, hierarchy_tbl, gran_final) - 1)
+    }
+  }
+  return(value)
+
+}
+
+
+validate_gran <-  function(.data, hierarchy_tbl = NULL, gran = NULL, validate_col = NULL, ...)
+{
+  all_gran <- dynamic_search_gran(.data, hierarchy_tbl)
+
+  if(!(gran %in% all_gran))# which granularity needs to be checked
+  {
+    stop("granularity to be validated needs to be one that can be formed from the hierarchy table.")
+  }
+  if(!(validate_col %in% names(.data)))  # column of data which has the granularity
+  {
+    stop("validate_col should be one of the columns of the data")
+  }
+
+  gran_data <- dynamic_create_gran(.data, hierarchy_tbl, gran)
+
+  data_col <- .data[[validate_col]]
+
+  if(all.equal(data_col, gran_data)==TRUE)
+    return(TRUE)
+  else(FALSE)
+}
+
 
 
 
@@ -45,60 +100,6 @@ create_single_gran <- function(.data, hierarchy_tbl = NULL, gran = NULL)
   circular_gran <-  dplyr::if_else(linear_gran %% denom == 0, denom, linear_gran %% denom)
 
 
-}
-
-dynamic_create_gran <-  function(.data, hierarchy_tbl = NULL, gran = NULL, verify_col = FALSE, ...)
-{
-
-  x = .data[[tsibble::index(.data)]] # index column
-
-  if(any(class(x) %in% c("POSIXct", "POSIXt")))
-    value = create_gran(.data, gran,...)
-  else
-  {
-  gran_split <- stringr::str_split(gran, "_", 2) %>% unlist() %>% unique()
-  lgran = gran_split[1]
-  ugran = gran_split[2]
-
-lgran_ordr1 <- dynamic_g_order(hierarchy_tbl, lgran, order = 1)
-
-gran_init <- paste(gran_split[1], lgran_ordr1, sep="_")
-gran_final <- paste(lgran_ordr1, gran_split[2], sep="_")
-
-if (dynamic_g_order(hierarchy_tbl, lgran, gran_split[2]) == 1) {
-  value =  create_single_gran(.data, hierarchy_tbl, gran)
-}
- else {
-  value <- dynamic_create_gran(.data, hierarchy_tbl, gran_init) +
-    dynamic_gran_convert(hierarchy_tbl, lgran, lgran_ordr1) *
-    (dynamic_create_gran(.data, hierarchy_tbl, gran_final) - 1)
- }
-}
-return(value)
-
-}
-
-
-validate_gran <-  function(.data, hierarchy_tbl = NULL, gran = NULL, validate_col = NULL, ...)
-{
-  all_gran <- dynamic_search_gran(.data, hierarchy_tbl)
-
-  if(!(gran %in% all_gran))# which granularity needs to be checked
-     {
-       stop("granularity to be validated needs to be one that can be formed from the hierarchy table.")
-  }
-  if(!(validate_col %in% names(.data)))  # column of data which has the granularity
-  {
-       stop("validate_col should be one of the columns of the data")
-  }
-
-  gran_data <- dynamic_create_gran(.data, hierarchy_tbl, gran)
-
-  data_col <- .data[[validate_col]]
-
-  if(all.equal(data_col, gran_data)==TRUE)
-    return(TRUE)
-  else(FALSE)
 }
 
 
