fb1e472d5604a69aaffc3d9237a28319f4f7e6e0
Sayani07 <Sayani.Gupta@monash.edu>
2019-10-10 10:55:36 +1100

    deleted documentation for 50 customers

--- R/data.R
+++ R/data.R
@@ -18,21 +18,6 @@
 #' }
 "sm_cust1"
 
-#' #' Smart meter data for 50 households
-#' #'
-#' #' Customer Trial data conducted as part of
-#' #' Smart Grid Smart City (SGSC) project
-#' #' (2010-2014) based in Newcastle,
-#' #' New South Wales
-#' #' and areas in Sydney.
-#' #' It contains half hourly interval meter readings (KWh)
-#' #' of electricity consumption
-#' #' and generation of households.
-#' #' The data is obtained from url{https://data.gov.au/dataset/ds-dga-4e21dea3-9b87-4610-94c7-15a8a77907ef/details?q=smart-meter}
-#' #'
-#' #' @usage data(sm_cust50)
-#' "sm_cust50"
-
 
 #' Cricket data for IPL 2008
 #'
