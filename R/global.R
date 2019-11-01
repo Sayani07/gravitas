23c88fb7050d38b3cf4b55033ab037213dff2607
Sayani07 <Sayani.Gupta@monash.edu>
2019-11-01 14:44:42 +1100

    deleting global variable to check which needs to be included

--- R/global.R
+++ R/global.R
@@ -1,5 +1,31 @@
 # to appease R CMD check
 # most of these are in R/app.R
 utils::globalVariables(
-  c("L1", "L2", "n", ":=", "nobs", "count", "combn", "V1", "y", "output", "x", "check_harmony", "Cat1", "V2", ".", "min_c", "max_c", "..LV..", "data_mutate[[gran1]]", "data_mutate[[gran2]]", "value", "granularity2", "ymin", "ymax", "data_mutate_obj", "gini_meas", "total_gini_meas", "quantiles")
+  c("L1",
+    "L2",
+    "n",
+    ":=",
+    "nobs",
+    "count",
+    "combn",
+    "V1",
+    "y",
+    "output",
+    "x",
+    "check_harmony",
+    "Cat1",
+    "V2",
+    ".",
+    "min_c",
+    "max_c",
+    "..LV..",
+    "data_mutate[[gran1]]",
+    "data_mutate[[gran2]]",
+    "value
+    granularity2",
+    "ymin", "ymax",
+    "data_mutate_obj",
+    "gini_meas",
+    "total_gini_meas",
+    "quantiles")
 )
