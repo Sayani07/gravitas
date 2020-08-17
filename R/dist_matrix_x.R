85b722c9080013c8901ad8b865975d54dedcafee
Sayani07 <Sayani.Gupta@monash.edu>
2020-08-17 12:00:28 +1000

    distance across x categories computed

--- R/dist_matrix_x.R
+++ R/dist_matrix_x.R
@@ -1,6 +1,7 @@
 # distance matrix between categories across x-axis
 
 # x: vector/matrix of multiple observations across categories
+# x = step1_data[[16]][-1][1,]
 quantile_extractx_n <-function(x,
                                prob = seq(0.01, 0.99, by = 0.01)){
   names(x) <- paste0("L", names(x))
@@ -16,76 +17,64 @@ quantile_extractx_n <-function(x,
     })
 }
 
-JSdist_pair_matrix <- function(x,
-                               dist_distribution = "normal",
-                               dist_ordered = TRUE)
+# y = quantile_extractx_n(x)
+JSdist_pair_matrix <- function(y,
+                               dist_ordered = TRUE,
+                               ...){
+  nrowy <- nrow(y)
 
-
-
-
-
-  step2 <- NULL
-  for (i in 1:lencol) {
-    step2[[i]] <- lapply(x[[colNms[i]]], quantile_extractx)
-  }
-
-  step3 <- rep(list(diag(lenrow)), lencol)
-
-  step4 <- p <- a <- b <- mu <- sigma <- array(NA, dim = lencol)
-
-  dist_vector <- vector()
-  ## Logic
-  # __ find the stepped sum difference of density vector elements
-    dist <- matrix(NA,
-                   nrow = lenrow,
-                   ncol = lenrow
-    ) ## Matrix
-    row_of_col_max <- NULL
-    for (i in 1:(lenrow - 1))
-    {
-      for (j in (i + 1):lenrow)
-      {
-        m1 <- step2[[k]][[i]]
-        m2 <- step2[[k]][[j]]
-        dist[i, j] <- JS(prob, m1, m2)
-        dist[dist == 0] <- NA
+dist <- (1:(nrowy - 1)) %>%
+  purrr::map(function(i){
+    ((i + 1):nrowy) %>%
+      purrr::map(function(j){
+        m1 <- y[i,]
+        m2 <- y[j,]
+        z = JS(prob, m1, m2)
         if (dist_ordered) {
-          if (j != i + 1) dist[i, j] <- NA
+          if (j != i + 1)
+         z = NA
         }
-      }
-    }
+ return(z)
+  })
+  })
 }
 
 # algorithm for normalisation
-JSdist_matrix_normalise <-
-
+# z = JSdist_pair_matrix(y)
+JSdist_normalise_matrix <- function(z,
+                                    dist_distribution = 'normal')
+{
+    dist <- unlist(z)
     max_dist <- max(dist, na.rm = TRUE)
 
-    dist[lower.tri(dist)] <- NA
-    len_uniq_dist <- lenrow^2 - length(which(is.na(dist)))
-    p[k] <- (1 - 1 / len_uniq_dist)
+    len_uniq_dist <- length(which(!is.na(dist)))
+    p <- (1 - 1 / len_uniq_dist)
 
-    mu[k] <- mean(dist, na.rm = TRUE)
-    sigma[k] <- stats::sd(dist, na.rm = TRUE)
-
-    if (dist_distribution == "general") {
-      a[k] <- stats::quantile(as.vector(dist), prob = 1 - p[k], type = 8, na.rm = TRUE)
-      step4[k] <- max_dist / a[k]
-    }
+    mu <- mean(dist, na.rm = TRUE)
+    sigma <- stats::sd(dist, na.rm = TRUE)
 
     if (dist_distribution == "normal") {
-      b[k] <- stats::qnorm(p = p[k], mean = mu[k], sd = sigma[k])
-      a[k] <- 1 / (len_uniq_dist * stats::dnorm(b[k], mean = mu[k], sd = sigma[k]))
-      step4[k] <- dplyr::if_else(len_uniq_dist == 1, mu[k], (max_dist - b[k]) / a[k])
+      b <- stats::qnorm(p = p, mean = mu, sd = sigma)
+      a <- 1 / (len_uniq_dist * stats::dnorm(b, mean = mu, sd = sigma))
+      value <- dplyr::if_else(len_uniq_dist == 1, mu, (max_dist - b) / a)
     }
-
-    d <- as.vector(dist)
-    d <- d[!is.na(d)]
-    dist_vector <- rbind(dist_vector, d)
+    return(value)
   }
 
-  row.names(dist_vector)
+dist_matrix_x <- function(harmony_datai){
 
-  value <- list(val = nmax_nmax, distvec = dist_vector, max_distance = max_max)
-  value
+# iterate for all facet_levels
+iter <- nrow(harmony_datai)
+
+(1:iter) %>%
+    purrr::map(function(i){
+      harmony_datai %>%
+        dplyr::slice(i) %>%
+        dplyr::select(-1) %>%
+        quantile_extractx_n() %>%
+        JSdist_pair_matrix() %>%
+        JSdist_normalise_matrix()
+    })
 }
+
+#dist_matrix_x(harmony_datai)
