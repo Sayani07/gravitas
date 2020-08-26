7f7b7ce30777eb775aba5f3cb0871518dab1739a
Sayani07 <Sayani.Gupta@monash.edu>
2020-08-27 08:12:49 +1000

    styler + cleaning

--- R/sim_distharmony1.R
+++ R/sim_distharmony1.R
@@ -11,29 +11,34 @@
 #' library(distributional)
 #' library(magrittr)
 #' library(tidyr)
-#' harmonies <- tibble::tibble(facet_variable =
-#'  c("A", "B"),
-#' x_variable  = c("B","A"), facet_levels = c(2, 3),x_levels = c(3, 2))
-#'.data = harmonies[1,]
-#'data <- sim_distharmony1(.data,
-#'  sim_dist = c(rep(dist_normal(mu = 2, sigma = 5),3),
-#'  rep(dist_normal(mu = 5, sigma = 10), 3)))
+#' harmonies <- tibble::tibble(
+#'   facet_variable =
+#'     c("A", "B"),
+#'   x_variable = c("B", "A"), facet_levels = c(2, 3), x_levels = c(3, 2)
+#' )
+#' .data <- harmonies[1, ]
+#' data <- sim_distharmony1(.data,
+#'   sim_dist = c(
+#'     rep(dist_normal(mu = 2, sigma = 5), 3),
+#'     rep(dist_normal(mu = 5, sigma = 10), 3)
+#'   )
+#' )
 #' @export sim_distharmony1
 
 sim_distharmony1 <- function(.data,
                              ntimes = 500,
-                             sim_dist = distributional::dist_normal(mu = 2, sigma = 5),...){
-
+                             sim_dist = distributional::dist_normal(mu = 2, sigma = 5), ...) {
   nfacet <- .data$facet_levels
   nx <- .data$x_levels
 
-  len_sim_dist <- sim_dist %>% lengths %>% length()
+  len_sim_dist <- sim_dist %>%
+    lengths() %>%
+    length()
 
   # if only one distribution is given, repeat it for all categories
 
-  if(len_sim_dist==1)
-  {
-    sim_dist = rep(sim_dist, nfacet*nx)
+  if (len_sim_dist == 1) {
+    sim_dist <- rep(sim_dist, nfacet * nx)
   }
 
   lev_facet <- paste0(.data$facet_variable, 1:nfacet)
@@ -41,9 +46,9 @@ sim_distharmony1 <- function(.data,
 
   data_1 <- expand.grid(lev_facet, lev_x) %>%
     dplyr::mutate(dist = sim_dist)
-  #sim_dist <- dist_normal(mu = 1:6, sigma = 3)
+  # sim_dist <- dist_normal(mu = 1:6, sigma = 3)
 
-  data_1 %>% dplyr::group_by(Var1, Var2)  %>%
+  data_1 %>%
+    dplyr::group_by(Var1, Var2) %>%
     dplyr::mutate(sim_dist = distributional::generate(dist, ntimes))
 }
-
